2f8ce14c473f49dadabe6e9e77e42b75cf152ae8
hyder.ali <hyder.ali@zohocorp.com>
2017-02-20 19:46:36 +0530

    Remove folder delete

--- app/services/session.js
+++ app/services/session.js
@@ -1,18 +1,13 @@
 import Ember from 'ember';
 const { computed: { equal } } = Ember;
 export default Ember.Service.extend({
-  isLoggedIn: true,
-  user: {
-    "username": "admin",
-    "password": "admin",
-    "authtoken": "38d7b247270149d2b6f7747823e5afc2",
-    "is_admin": true
-  },
+  isLoggedIn: false,
+  user: null,
   itemslist: null,
-  organization_id: "639052220",
-  customer_id: "512244000000052446",
+  organization_id: null,
+  customer_id: null,
   vendors: [],
-  inventory_account_id: "512244000000034001",
-  cogs_id: "512244000000034003",
+  inventory_account_id: null,
+  cogs_id: null,
   isAdmin: equal('user.is_admin', true)
 });
