3e12256593a9e40213098daa176804f309ac03df
hyder.ali <hyder.ali@zohocorp.com>
2017-04-03 19:08:34 +0530

    Uses jsbarcode and bootstrap from npm

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,8 +18,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-  app.import('bower_components/jsbarcode/dist/JsBarcode.all.min.js');
+  app.import(app.nodeModulesDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+  app.import(app.nodeModulesDirectory + '/bootstrap/dist/js/bootstrap.min.js');
+  app.import(app.nodeModulesDirectory + '/jsbarcode/dist/JsBarcode.all.min.js');
   return app.toTree();
 };
