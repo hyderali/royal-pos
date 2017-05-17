0a0a4a39ae252f1ca5caf207ab1cc5dd024c8665
hyder.ali <hyder.ali@zohocorp.com>
2017-05-17 19:39:04 +0530

    filters active items only

--- app/routes/application.js
+++ app/routes/application.js
@@ -11,7 +11,7 @@ export default Route.extend({
   afterModel() {
     if (!this.get('session.itemslist')) {
       return this.get('store').ajax('/itemslist').then((json) => {
-        this.set('session.itemslist', json.items);
+        this.set('session.itemslist', json.items.filterBy('Status', 'Active'));
         this.set('session.customer_id', json.customer_id);
         this.set('session.organization_id', json.organization_id);
       });
--- app/routes/sales.js
+++ app/routes/sales.js
@@ -16,7 +16,7 @@ export default Route.extend({
     }
     if (!this.get('session.itemslist')) {
       return this.get('store').ajax('/itemslist').then((json) => {
-        this.set('session.itemslist', json.items);
+        this.set('session.itemslist', json.items.filterBy('Status', 'Active'));
         this.set('session.customer_id', json.customer_id);
         this.set('session.organization_id', json.organization_id);
       });
