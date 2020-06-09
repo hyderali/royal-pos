92c6e6c622ed7f8ebec6389ac806ee908139bfc4
hyder.ali <hyder.ali@zohocorp.com>
2020-06-09 19:30:03 +0530

    Show IP address

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const os = require('os');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -22,5 +23,26 @@ module.exports = function(defaults) {
   app.import('node_modules/bootstrap/dist/css/bootstrap.min.css');
   app.import('node_modules/bootstrap/dist/js/bootstrap.min.js');
   app.import('node_modules/jsbarcode/dist/JsBarcode.all.min.js');
+  let ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(`Serving on http://${iface.address}:4200`);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(`Serving on http://${iface.address}:4200`);
+    }
+    ++alias;
+  });
+});
   return app.toTree();
 };
