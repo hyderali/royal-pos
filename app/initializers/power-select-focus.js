0df794382874c2efae7d7d9428be7ed813559ebb
hyder.ali <hyder.ali@zohocorp.com>
2021-08-15 16:20:39 +0530

    Updates Ember to 3.14

--- app/initializers/power-select-focus.js
+++ app/initializers/power-select-focus.js
@@ -1,17 +1,17 @@
-import { next } from '@ember/runloop';
-import EmberPowerSelect from 'ember-power-select/components/power-select';
+// import { next } from '@ember/runloop';
+// import EmberPowerSelect from 'ember-power-select/components/power-select';
 export function initialize(/* application */) {
-    EmberPowerSelect.reopen({
-        didInsertElement: function() {
-            this._super(...arguments);
-            next(() => {
-                if(this.get('autofocus')) {
-                  this._super(...arguments);
-                  document.querySelector(`[data-ebd-id=${this.publicAPI.uniqueId}-trigger]`).focus();
-                }
-            });
-        }
-    });
+    // EmberPowerSelect.reopen({
+    //     didInsertElement: function() {
+    //         this._super(...arguments);
+    //         next(() => {
+    //             if(this.get('autofocus')) {
+    //               this._super(...arguments);
+    //               document.querySelector(`[data-ebd-id=${this.publicAPI.uniqueId}-trigger]`).focus();
+    //             }
+    //         });
+    //     }
+    // });
 }
 export default {
   initialize
