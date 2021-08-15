4ba098334ed2cd15dd726dca36dac26b28e1e18e
hyder.ali <hyder.ali@zohocorp.com>
2021-08-15 16:31:12 +0530

    Changes org details as dynamic

--- app/components/sales-receipt.js
+++ app/components/sales-receipt.js
@@ -1,5 +1,7 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 export default Component.extend({
+  session: service(),
   numberLabel: 'Bill No',
   isSales: true
 });
