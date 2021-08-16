b49490f57e5189123d2a86ae8d542c06866bd6e7
hyder.ali <hyder.ali@zohocorp.com>
2021-08-16 11:47:52 +0530

    Fix lint issues

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'no-inline-styles': false,
+    'no-unknown-arguments-for-builtin-components': false,
+    'no-implicit-this': false,
+    'no-action': false,
+    'require-input-label': false,
+    'no-curly-component-invocation': false,
+    'no-invalid-interactive': false,
+    'no-yield-only': false,
+    'no-obsolete-elements': false,
+  }
 };
