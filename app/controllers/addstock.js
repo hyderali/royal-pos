9b7a13aa6049b6c857e7527ae09bacfa073f293a
hyder.ali <hyder.ali@zohocorp.com>
2021-08-18 15:51:44 +0530

    Update quantity manually

--- app/controllers/addstock.js
+++ app/controllers/addstock.js
@@ -1,17 +1,13 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
 export default Controller.extend({
-  total: computed('items.@each.InitialStock', {
-    get() {
+  actions: {
+    computeTotal() {
       let items = this.items;
       let total = 0;
       items.forEach((item) => {
-        total += item['Initial Stock'];
+        total += Number(item['Initial Stock']);
       });
-      return total;
-    },
-    set(key, value) {
-      return value;
+      this.set('total', total);
     }
-  }),
+  }
 });
