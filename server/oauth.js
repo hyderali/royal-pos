00fe0da52e05a6a40c9da0d6151f26b18da40226
hyder.ali <hyder.ali@zohocorp.com>
2025-09-28 14:54:48 +0530

    Remove reuse access_token

--- server/oauth.js
+++ server/oauth.js
@@ -16,13 +16,6 @@ module.exports = {
     var REDIRECT_URI = 'https://www.zoho.com/books';
     let self = this;
     return new RSVP.Promise((resolve, reject) => {
-      if (user.access_token) {
-        console.log(
-          `Returning already generated token for user - ${user.username}\n\n`
-        );
-        resolve(user);
-        return;
-      }
       var url = `https://accounts.zoho.com/oauth/v2/token?refresh_token=${user.refresh_token}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&grant_type=refresh_token`
       console.log('Going to generate access token for user ', user.username);
       request.post({
